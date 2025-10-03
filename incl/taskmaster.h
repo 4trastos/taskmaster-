5bc55aa8224290d02c65d5be00ed8d7405594a77
4trastos <info@4trastos.com>
2025-10-03 16:19:29 +0200

    System Architecture

--- incl/taskmaster.h
+++ incl/taskmaster.h
@@ -13,6 +13,30 @@
 # include <sys/signal.h>
 # include <sys/user.h>
 
+/* cmd: "comando a ejecutar"
+numprocs: 1
+autostart: true
+autorestart: "unexpected"  # always, never, unexpected
+exitcodes: [0, 1]
+startretries: 3
+starttime: 5
+stopsignal: "SIGTERM"
+stoptime: 10
+stdout: "/path/to/stdout.log"
+stderr: "/path/to/stderr.log"
+env:
+  VAR1: "valor1"
+  VAR2: "valor2"
+workingdir: "/path/to/workdir"
+umask: "022" */
+
+typedef enum s_autorestart
+{
+    AR_ALWAYS,
+    AR_NEVER,
+    AR_UNEXPECTED
+}   t_autorestart;
+
 typedef enum s_process_state
 {
     STOPPED,            // Proceso detenido
@@ -27,9 +51,22 @@ typedef enum s_process_state
 
 typedef struct s_program_config
 {
-    char    *name;
-    char    *command;
-    char    **env;
+    char            *name;
+    char            *command;
+    char            **env;
+    char            *workingdir;
+    char            *stdout_path;
+    char            *stderr_path;
+    int             *exitcodes;
+    int             numbexicod;
+    int             numprocs;
+    int             starttime;
+    int             stoptime;
+    int             stopsignal;
+    int             startretries;
+    bool            autostart;
+    mode_t          umask;
+    t_autorestart   autorestart;
 }   t_program_config;
 
 typedef struct s_process
